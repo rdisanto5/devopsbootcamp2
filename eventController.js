1db6b359ea1754339d120f5065831fbee010fb20
Rob DiSanto <rodisanto@deloitte.com>
2019-08-21 09:42:07 -0400

    initial commit

---
+++ eventController.js
@@ -0,0 +1,32 @@
+exports.events=function(){
+
+let eventObj1 = {"eventID":001,
+            "eventName": "DevOps Bootcamp 1",
+            "location":"Boston",
+            "date":"08-21-2019"
+}
+
+let eventObj2 = {"eventID":002,
+            "eventName": "DevOps Bootcamp 2",
+            "location":"Philadelphia",
+            "date":"08-22-2019"
+}
+
+let eventObj3 = {"eventID":003,
+            "eventName": "DevOps Bootcamp 3",
+            "location":"New York",
+            "date":"08-23-2019"
+}
+
+let eventList = [eventObj1, eventObj2, eventObj3];
+
+return eventList;
+}
+
+
+exports.version = function(){
+
+let version = "1.0"
+
+return version;
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const controller = require('./eventController')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/events', (req, res) => {
+    let x = controller.events()
+    let text = "";
+    for (let i = 0; i < x.length; i++) { 
+    text += "<br>" + "<br>" + x[i].eventName + "<br>"
+    text += x[i].location + "<br>"
+    text += x[i].date + "<br>";
+}
+    res.set('Content-Type', 'text/html');
+    res.send(new Buffer(`All Events: ${text}`));
+    })
+
+app.get('/eventsversion', (req, res) => {
+    let x = controller.version()
+    res.send(`This is version ${x}`)
+    })
+
+app.get('/eventscount', (req, res) => {
+    let x = controller.events()
+    res.send(`There are ${x.length} events`)
+    })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
