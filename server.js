08b85655c848bbcd928487d277da41622814c7af
Rob DiSanto <rodisanto@deloitte.com>
2019-08-23 15:16:18 -0400

    Test A

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const controller = require('./eventController')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Goodbye!'))
+app.get('/', (req, res) => res.send('Demo Test A'))
 
 app.get('/events', (req, res) => {
     let x = controller.events()
